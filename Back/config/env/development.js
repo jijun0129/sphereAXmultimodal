63080c402083f23ad50a3b7e455312d19c4a9c45
machi2001 <ckdwh277@gmail.com>
2024-10-03 19:09:39 +0900

    Feat: 백엔드 회원가입, 로그인, 로그아웃 기능 추가

--- Back/config/env/development.js
+++ Back/config/env/development.js
@@ -1,4 +1,4 @@
 module.exports = {
-    db: 'mongodb://localhost:27017/VP_reid_v2',
-    UsertokenSecret:'mv\5ecr3t' 
+    db: 'mongodb://mongodb:27017/VP_reid_v2',
+    UsertokenSecret:'mv\5ecr3t'
 };
--- Back/server.js
+++ Back/server.js
@@ -1,6 +1,7 @@
 
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'production';
+const { Server } = require('socket.io');
 var express = require('./config/express_config')
   , mongoose = require('./config/mongoose_config')
   , user_passport = require('./config/passport/user_passport');
@@ -11,9 +12,20 @@ var app = express()
   , user_passport = user_passport()
   , port = process.env.PORT || 10111;
 
+
 global.socketSalt = '';
 // global.socketCli = require('socket.io-client')('http://localhost:' + port, {secure: true, rejectUnauthorized: false})
 global.socketCli = require('socket.io-client')('http://localhost:' + port)
+
+app.on('request', (req, res) => {
+  if (req.method === 'GET' && req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('hello world');
+    next();
+  }
+});
+
+
 app.listen(port, function (err) {
   if (!err) {
     console.log("server start");
@@ -28,5 +40,6 @@ app.listen(port, function (err) {
     console.log("server open err");
   }
 });
+
 
 
