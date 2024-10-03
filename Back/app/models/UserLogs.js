73b747576562a5c2de9a1cc3eef2527f093bc6b4
machi2001 <111967267+machi2001@users.noreply.github.com>
2024-10-03 19:26:27 +0900

    Revert "Feat: 백엔드 회원가입, 로그인, 로그아웃 기능 추가"
    This reverts commit 63080c402083f23ad50a3b7e455312d19c4a9c45.

--- Back/app/models/UserLogs.js
+++ Back/app/models/UserLogs.js
@@ -1,30 +1,30 @@
 'use strict';
 
-var mongoose = require('mongoose'),
-	Schema = mongoose.Schema;
+var mongoose = require('mongoose'), 
+	Schema = mongoose.Schema;  
 
 var moment = require('moment');
 
-var UserLogs = new Schema({
+var UserLogs = new Schema({  
+    
+		created: {
+			type: new Date()
+			
+		},
+		event: {
+			type: String
+		},
+		IPAddress: {
+			type: String
+		},
+		ID : {
+			type: String
+		}
+		
+	});
 
-	created: {
-		type: Date,        // 타입은 Date로 지정
-		default: Date.now  // 기본값으로 현재 시간을 설정
-	},
-	event: {
-		type: String
-	},
-	IPAddress: {
-		type: String
-	},
-	ID: {
-		type: String
-	}
 
-});
-
-
-mongoose.model('UserLogs', UserLogs);
+mongoose.model('UserLogs', UserLogs);  
 
 
 
--- Back/server.js
+++ Back/server.js
@@ -1,7 +1,6 @@
 
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'production';
-const { Server } = require('socket.io');
 var express = require('./config/express_config')
   , mongoose = require('./config/mongoose_config')
   , user_passport = require('./config/passport/user_passport');
@@ -12,20 +11,9 @@ var app = express()
   , user_passport = user_passport()
   , port = process.env.PORT || 10111;
 
-
 global.socketSalt = '';
 // global.socketCli = require('socket.io-client')('http://localhost:' + port, {secure: true, rejectUnauthorized: false})
 global.socketCli = require('socket.io-client')('http://localhost:' + port)
-
-app.on('request', (req, res) => {
-  if (req.method === 'GET' && req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('hello world');
-    next();
-  }
-});
-
-
 app.listen(port, function (err) {
   if (!err) {
     console.log("server start");
@@ -40,6 +28,5 @@ app.listen(port, function (err) {
     console.log("server open err");
   }
 });
-
 
 
