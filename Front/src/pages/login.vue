1fee4160cffecd097be76833edd665bcbb14af3d
Injun Jeong <jijun897@naver.com>
2024-10-13 23:12:17 +0900

    Feat(FE): 웹페이지 헤더 구현

---
+++ Front/src/pages/login.vue
@@ -0,0 +1,7 @@
+<template>
+	<the-header :isLoggedIn="false"></the-header>
+	<div>login page</div>
+</template>
+<script setup>
+import TheHeader from '../components/layout/TheHeader.vue';
+</script>
